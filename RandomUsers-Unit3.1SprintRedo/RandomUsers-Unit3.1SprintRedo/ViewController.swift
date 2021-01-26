f547afa343aafa700bb225b61b96d8d7925b412d
brys <brys1saclaus2@gmail.com>
2021-01-25 19:08:44 -0800

    Project start

---
+++ RandomUsers-Unit3.1SprintRedo/RandomUsers-Unit3.1SprintRedo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUsers-Unit3.1SprintRedo
+//
+//  Created by BrysonSaclausa on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
